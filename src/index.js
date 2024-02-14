1936458b423e844e2c8e6dd740fb5e128da07881
lylyrosy <lylyrose51@gmail.com>
2024-02-14 16:13:37 +0100

    worker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+
 import { HelmetProvider } from 'react-helmet-async';
 
 
@@ -15,7 +18,7 @@ root.render(
     
   </React.StrictMode>
 );
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
