87228d4fe1d3d40c3488f81b6e29320e5abc7280
lylyrosy <lylyrose51@gmail.com>
2024-02-15 11:15:53 +0100

    commit routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ import { FaChrome } from 'react-icons/fa';
 
 import { Helmet } from 'react-helmet-async';
 
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
+import Indexx from './pages/indexx';
+import Bienvenue from './pages/bienvenue'
 
 
 
@@ -23,90 +26,29 @@ function App() {
 
     <div>
 
-
-<Helmet>
-
-<link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Gruppo&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Teko:wght@300..700&display=swap" rel="stylesheet"/>
+<h1> site en reparation</h1>
 
 
+<Bienvenue/>
 
-<link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet"/>
+      <BrowserRouter>
+      
+      <Routes>
 
-<link href="https://fonts.googleapis.com/css2?family=Gruppo&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Teko:wght@300..700&display=swap" rel="stylesheet"/> 
+      <Route index element ={<Bienvenue/>} />
 
-
-<link rel="stylesheet" href="./styles/bienvenue.css"/>
-
-
-
-</Helmet>
-
-<div className="container">
-<h1>
-Bienvenue <br />
-Fais comme chez toi
-</h1>
-<p className="design">
-Designed by Lili Rose- 100% free à but caritatif
-<br />
-<br />
-Conjointement avec Dr 
-<strong>SIF.Zahia</strong> Gynécologue-Obstétricienne <br />
-<br />
-Pour accompagner toute personne atteinte d'un stress post-traumatique
-</p>
-<p>
-<strong>Instructions setup/use</strong>
-</p>
-
-<code style={{ textAlign: "center" }}>
-  <strong>
-    {" "}
-    &nbsp; Ouvrez dans chrome <FaChrome/>
-  </strong>
-  <br />
-</code>
-<code>
-<strong>Trouvez dans la barre d'adresse </strong>
-</code>
-<br />
-<code>
-<strong>
-  {" "}
-  <span className="points"> ⁝ </span> INSTALLER (mobile)
-</strong>
-</code>
-<code>
-<strong>
-  {" "}
-  <span className="points">💻</span>INSTALLER (desktop){" "}
-</strong>
-</code>
-
-<code style={{ textAlign: "center" }}>
-{" "}
-&nbsp; alt+ <span className="points">🔙</span>(retour desktop)
-</code>
+      <Route path='/indexx' element ={<Indexx/>} />
 
 
+      </Routes>
+      
+      
+      
+      
+      
+      </BrowserRouter>
 
-<br />
-<p>
-<span style={{ letterSpacing: 1, color: "rgb(101, 5, 47)" }}>
-  {" "}
-  Pour consulter Lili Rose &nbsp;
-</span>
-<a href="./indexx.html">Cliquez ici </a>
-</p>
-<p className="groupo">
-Mon kif pour l'informatique m'accompagne sur le chemin qui mène à la paix...
-</p>
-<code>Lili Rose Version 1.1 Beta</code>
-</div>
+
 
 
 
---
+++ src/pages/indexx.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+export default function indexx() {
+  return (
+    <div>
+
+
+<h1> desolée site en réparation </h1>
+
+    </div>
+  )
+}
