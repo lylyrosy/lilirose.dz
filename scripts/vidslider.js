55be0f3a801a9911bbb3c119233073344714f61c
lylyrosy <lylyrose51@gmail.com>
2024-02-23 17:32:59 +0100

    Updates

--- scripts/vidslider.js
+++ scripts/vidslider.js
@@ -78,7 +78,7 @@ i--;
 }
 
 
-//  let stop= setInterval(monruban,20)
+  let stop= setInterval(monruban,25)
 
 
   
@@ -104,58 +104,63 @@ ibalise.addEventListener("click", (params) => {
 
 
 
-  // film.addEventListener("click", (params) => {
+  film.addEventListener("click", (params) => {
 
 
-  // if (film.classList.contains("pause")) 
+  if (film.classList.contains("pause")) 
   
-  // {
-  //   clearInterval(stop); film.classList.toggle("pause");
+  {
+    clearInterval(stop); film.classList.toggle("pause");
 
  
 
-  //  } 
+   } 
    
    
-  //  else
-  //  {
+   else
+   {
 
     
-  //   film.classList.toggle("pause")
-  //   stop=setInterval(monruban, 25)
-  // console.log(stop)
+    film.classList.toggle("pause")
+    stop=setInterval(monruban, 25)
+  console.log(stop)
 
-  // }
+  }
     
-  // })
+  })
  
 
-  let stop= setInterval(monruban,15)
 
 
-  setTimeout(() => {
+
+
+
+//   let stop= setInterval(monruban,15)
+
+
+//   setTimeout(() => {
  
- clearInterval(stop)
+//  clearInterval(stop)
  
- }, 9000);
- 
- 
+//  }, 9000);
  
  
  
-   film.addEventListener("click", (params) => {
+ 
+ 
+//    film.addEventListener("click", (params) => {
  
  
  
     
      
  
-     let stop= setInterval(monruban,15)
+//      let stop= setInterval(monruban,15)
  
  
-     setTimeout(() => {
+//      setTimeout(() => {
     
-    clearInterval(stop)
+//     clearInterval(stop)
     
-    }, 7000);
-  })
+//     }, 7000);
+//   })
